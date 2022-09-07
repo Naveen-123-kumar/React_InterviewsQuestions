bb47b32e8429c253de687973b8d0aefe07a4114a
NAVEEN KUMAR <1998naveenk@gmail.com>
2022-09-07 19:38:13 +0530

    Day two has been updated

---
+++ Day2/Day2.js
@@ -0,0 +1,18 @@
+// 1)What is the Difference between react and react native? Which one is library or framework?
+// Ans->React and React Native are developed by Facebook and become most popular in very short TimeRanges.React is known as ReactJs, which is a JavaScript library to build single-page web applications whereas React Native is a React JS-based framework to design mobile apps.React is JavaScript library and React Native is react framework.
+
+// 2)What is the package name you are using for routing?
+// Ans->React-router-dom is used for routing in react js 
+
+// 3).How do you pass data from parent to child?
+// Ans->We pass data from parent to child using props in functional components and state in class components. 
+
+// 4)What is lazy loading in react ?
+// Ans->lazy loading means that a component or a part of code must get loaded when it is required. It is also referred to as code splitting and data fetching
+
+// 5)Difference b/w Stateful and stateless Component?
+// Ans->A stateful component is a component that holds some state. Stateless components, by contrast, have no state. Stateful component can contains the state object and event handling function, user actions as well.Stateless component are pure in nature which does a very specific task.
+
+// 6)How do you switch one component to another, Conditional Rendering?
+// Ans->We can switch from one component to another by routing.Conditional rendering means if we have to component and there is some condition and that is true then first component will execute else second one will execute.
+// for Example->(condition)?<Component1/>:<Component2/>
